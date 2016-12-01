88d0b1e49da019fd24382c04db1dfae96575d123
kosasihdev <kosasih.dev@gmail.com>
2016-12-01 15:21:55 +0700

    Commit Pertama

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<html> <body> <h1>Halo</h1> Selamat datang di webapp saya.<br> Laravel, emang keren banget!
+{!! Form::open(['url' => 'post/save']) !!}
+//
+
+{!! Form::text('username') !!}
+{!! Form::close() !!}
+
+
+ </body> </html>
---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+Route::get('/', 'GuestController@index');
+
+Route::get('/about','MyController@showAbout');
+
+Route::get('/testmodel', function() { $query = App\Post::all();  return $query; });
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => 'web'], function () {
+
+Route::group(['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function () {
+
+Route::resource('authors', 'AuthorController'); 
+Route::resource('books', 'BookController');
+Route::resource('members', 'MembersController');
+	
+
+ // Route diisi disini...
+
+
+  });
+
+  });
+
+Route::get('books/{book}/borrow', [
+'middleware' => ['auth', 'role:member'],
+'as' => 'guest.books.borrow',
+'uses' => 'BookController@borrow'
+]);
+
+
+Route::put('books/{book}/return', [
+'middleware' => ['auth', 'role:member'],
+'as' => 'member.books.return',
+'uses' => 'BookController@returnBack'
+]);
+
+Route::get('auth/verify/{token}', 'Auth\RegisterController@verify');
+
+Route::get('auth/send-verification', 'Auth\RegisterController@sendVerification');
+
+//route untuk semua logic profil
+Route::get('settings/profile', 'SettingsController@profile');
+Route::get('settings/profile/edit', 'SettingsController@editProfile');
+Route::post('settings/profile', 'SettingsController@updateProfile');
+
+Route::get('settings/password', 'SettingsController@editPassword');
+Route::post('settings/password', 'SettingsController@updatePassword');
+
+
